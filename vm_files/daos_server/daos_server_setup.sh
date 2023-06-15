999439441d12b80d98113d9c3f5bf2ba6df6179c
Mark Olson <115657904+mark-olson@users.noreply.github.com>
2023-06-14 17:07:25 -0700

    DAOSAZ-16 Increase sleep time in cloud-init script (#6)
    Increased the time that the code that runs on the first server waits
    before attempting to run `daos storage format` and
    `daos pool create`.
    
    This is to allow for all of the VMs in the scale set to be
    provisioned and to start the daos_server service.
    
    Signed-off-by: Mark Olson <115657904+mark-olson@users.noreply.github.com>

--- vm_files/daos_server/daos_server_setup.sh
+++ vm_files/daos_server/daos_server_setup.sh
@@ -2,29 +2,25 @@
 
 set -eo pipefail
 
-# trap 'error_handler' ERR
-# trap 'cleanup' SIGINT
+trap 'echo "daos_server_setup.sh: Unknown error occurred. Exiting' ERR
+
 echo "BEGIN: daos_server_setup.sh"
 
 SCRIPT_DIR="$(realpath "$(dirname $0)")"
 SCRIPT_FILE=$(basename "${BASH_SOURCE[0]}")
 SCRIPT_ENV_FILE="${SCRIPT_FILE%.*}.env"
 DAOS_BDEV_ADDR=$(lspci | grep 'Non-Volatile memory controller' | awk '{print $1}')
-DAOS_SERVER_SYSTEMD_FILE="/etc/systemd/system/daos_server.service"
-DAOS_SCM_SIZE=40
-DAOS_TIER_RATIO=3
-DAOS_POOL_SIZE=4TB
 
-# Source Environment Variables
-load_env() {
-  if [[ -f "${SCRIPT_ENV_FILE}" ]]; then
-    source "${SCRIPT_ENV_FILE}"
-  else
-    echo "ERROR: Could not source '${SCRIPT_ENV_FILE}'. File not found."
-    #error_handler
-    exit 1
-  fi
-}
+if [[ -f "${SCRIPT_ENV_FILE}" ]]; then
+  source "${SCRIPT_ENV_FILE}"
+else
+  echo "ERROR: Could not source '${SCRIPT_ENV_FILE}'. File not found."
+  echo "Default settings will be used"
+fi
+
+DAOS_TIER_RATIO="${DAOS_TIER_RATIO:=3}"
+DAOS_POOL_SIZE="${DAOS_POOL_SIZE:="100%"}"
+DAOS_POOL_NAME="${DAOS_POOL_NAME:="pool1"}"
 
 # HACK: Need to fix the waagent
 #       Disabling it for now because it makes a mess of /var/log/messages
@@ -48,11 +44,6 @@ gen_ap_list() {
     fi
   fi
 
-  # Slurm style host list not working right now
-  # See https://daosio.atlassian.net/browse/DAOS-13662
-  #DAOS_AP_LIST="${DAOS_VM_BASE_NAME}-[000000-$(printf "%06d" $(($max_aps - 1)))]"
-
-  # Have to generate access_points list that contains individual hosts
   for ((i = 0; i < max_aps; i++)); do
     padded_index=$(printf "%06d" $i)
     hostname="${DAOS_VM_BASE_NAME}-${padded_index}"
@@ -65,25 +56,14 @@ gen_ap_list() {
       DAOS_AP_LIST="${DAOS_AP_LIST},\"${hostname}\""
     fi
   done
-
 }
 
 gen_config() {
 
-  # readarray -t daos_vars < <(compgen -A variable | grep "DAOS" | sort)
-  # for var in "${daos_vars[@]}"; do
-  #   export "$var"
-  #   log.debug "Exported: $var=${daos_vars[$var]}"
-  # done
-  echo "DAOS_BDEV_ADDR=${DAOS_BDEV_ADDR}"
-  echo "DAOS_AP_LIST=${DAOS_AP_LIST}"
-  echo "DAOS_FIRST_SERVER=${DAOS_FIRST_SERVER}"
-
-  export DAOS_BDEV_ADDR
-  export DAOS_AP_LIST
-  export DAOS_FIRST_SERVER
-  export DAOS_AZ_serverCount
-  export DAOS_SCM_SIZE
+  readarray -t daos_vars < <(compgen -A variable | grep "DAOS" | sort)
+  for var in "${daos_vars[@]}"; do
+    export "$var"
+  done
 
   echo "Generating /etc/daos/daos_server.yml"
   envsubst <"${SCRIPT_DIR}/config/daos_server.yml.envtpl" >/etc/daos/daos_server.yml
@@ -97,24 +77,43 @@ start_service() {
   systemctl start daos_server
 }
 
-load_env
-disable_waagent
-gen_ap_list
-gen_config
-start_service
+prepare_storage() {
+  if [[ $(hostname -s) == "${DAOS_FIRST_SERVER}" ]]; then
+    # FIX: The following sleep statement assumes that 60 seconds is enough time
+    #      for all VMs in the scale set to be deployed and start the daos_server.
+    #      This is not a good solution. There is no guarantee that the VMs will
+    #      be ready in that amount of time. We need to replace this with a loop
+    #      around an API call to get the state of the VMs in the scale set and
+    #      ensure they are all up and the daos_server service is running before
+    #      we attempt to run `dmg storage format`.
+    #      See https://daosio.atlassian.net/browse/DAOSAZ-15
+    sleep 60
+    dmg storage format
+    dmg system query -v
+    # FIX: The following sleep statement was added because errors were occuring
+    # when pool create was run immediately after dmg system query returned.
+    # Needs investigation.
+    sleep 10
+    if [[ "${DAOS_POOL_SIZE}" == "100%" ]]; then
+      dmg pool create --size="${DAOS_POOL_SIZE}" "${DAOS_POOL_NAME}"
+    else
+      dmg pool create --size="${DAOS_POOL_SIZE}" --tier-ratio="${DAOS_TIER_RATIO}" "${DAOS_POOL_NAME}"
+    fi
+    # FIX: The following sleep statement was added because errors were occuring
+    # when pool update-acl was run immediately after dmg system query returned.
+    # Needs investigation.
+    sleep 10
+    dmg pool update-acl -e 'A::EVERYONE@:rcta' "${DAOS_POOL_NAME}"
+  fi
+}
 
-if [[ $(hostname -s) == "${DAOS_FIRST_SERVER}" ]]; then
-  sleep 20
-  dmg storage format
-  dmg system query -v
+main() {
+  disable_waagent
+  gen_ap_list
+  gen_config
+  start_service
+  prepare_storage
+  echo "END: daos_server_setup.sh"
+}
 
-  #DAOS_TOTAL_NVME_FREE="$(dmg storage query usage | awk '{split($0,a," "); sum += a[10]} END {print sum}')TB"
-  #echo "Total NVMe-Free: ${DAOS_TOTAL_NVME_FREE}"
-  # FIX: Need to calculate the pool size properly based on amount of storage and tier ratio
-  #      Right now there is not enough memory for SCM to use all the available disk space.
-  dmg pool create --size="${DAOS_POOL_SIZE}" --tier-ratio="${DAOS_TIER_RATIO}" pool1
-  dmg pool get-acl pool1
-  dmg pool update-acl -e 'A::EVERYONE@:rcta' pool1
-fi
-
-echo "END: daos_server_setup.sh"
+main
