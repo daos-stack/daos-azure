cd26b1bab34c0d115913de8703f7e532d710e281
Mark Olson <115657904+mark-olson@users.noreply.github.com>
2024-03-27 09:50:19 -0700

    DAOSAZ-29 update information about SSH config for tunnel (#15)
    Updated documentation regarding SSH tunnel to make it optional.
    Added parameter to tunnel.sh to only print the SSH configuration so that it can be manually added to ~/.ssh/config
    
    Signed-off-by: Mark Olson <115657904+mark-olson@users.noreply.github.com>

--- bin/tunnel.sh
+++ bin/tunnel.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Copyright (c) 2023 Intel Corporation All rights reserved.
+# Copyright (c) 2024 Intel Corporation All rights reserved.
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
@@ -11,10 +11,13 @@ SCRIPT_DIR="$(realpath "$(dirname "$0")")"
 SCRIPT_FILE=$(basename "${BASH_SOURCE[0]}")
 DEFAULT_ENV_FILE="${SCRIPT_DIR}/../daos-azure.env"
 DAOS_AZ_ENV_FILE="${DAOS_AZ_ENV_FILE:="${DEFAULT_ENV_FILE}"}"
+
 TUNNEL_ACTION=""
-TUNNEL_LOCAL_PORT=2022
-TUNNEL_SSH_CONFIG_FILE=~/.ssh/config.d/azure-tunnel
 TUNNEL_SSH_UPDATE_CONFIG="false"
+TUNNEL_LOCAL_PORT=2022
+TUNNEL_SSH_INCLUDE_DIR="${HOME}/.ssh/config.d"
+TUNNEL_SSH_CONFIG_FILE="${TUNNEL_SSH_INCLUDE_DIR}/azure-tunnel"
+
 
 # shellcheck disable=SC1091
 source "${SCRIPT_DIR}/_log.sh"
@@ -34,11 +37,12 @@ Usage:
 Options:
 
   [ -e | --env-file DAOS_AZ_ENV_FILE ]  Use specified environment file
-  [ -l | --list ]                       List tunnel(s)
-  [ -c | --create ]                     Create tunnel
-  [ -d | --delete ]                     Delete tunnel
-  [ --configure-ssh ]                   Update ~/.ssh/config with include path
-  [ -h | --help ]                       Show this help
+  [ -l | --list ]     List tunnel(s)
+  [ -c | --create ]   Create tunnel
+  [ -d | --delete ]   Delete tunnel
+  [ -p | --print ]    Print SSH configuration. Do not modify ~/.ssh/config
+  [ --configure-ssh ] Update ~/.ssh/config with include path
+  [ -h | --help ]     Show this help
 
 "
 
@@ -70,23 +74,19 @@ configure_ssh() {
     # shellcheck disable=SC2174
     [[ ! -d ~/.ssh ]] && mkdir -m 700 -p ~/.ssh
     # shellcheck disable=SC2174
-    [[ ! -d ~/.ssh/config.d ]] && mkdir -m 700 -p ~/.ssh/config.d
+    [[ ! -d "${TUNNEL_SSH_INCLUDE_DIR}" ]] && mkdir -m 700 -p "${TUNNEL_SSH_INCLUDE_DIR}"
     touch ~/.ssh/config && chmod 600 ~/.ssh/config
-    if ! grep -E -q 'Include\s+~/.ssh/config.d/\*' ~/.ssh/config; then
-      log.info "Adding 'Include ~/.ssh/config.d/*' to ~/.ssh/config"
-      echo 'Include ~/.ssh/config.d/*' >> ~/.ssh/config
+    if ! grep -E -q "Include\\s+${TUNNEL_SSH_INCLUDE_DIR}/\\*" ~/.ssh/config; then
+      log.info "Adding 'Include ${TUNNEL_SSH_INCLUDE_DIR}/*' to ~/.ssh/config"
+      echo "Include ${TUNNEL_SSH_INCLUDE_DIR}/*" >> ~/.ssh/config
     fi
   fi
 }
 
 create_ssh_config() {
-  mkdir -p "${HOME}/.ssh/ctrl"
+  local ssh_config_file="$1"
 
-  log.info "Creating SSH config: '${TUNNEL_SSH_CONFIG_FILE}'"
-
-  # This assumes you have 'Include ~/.ssh/config.d/*' in your ~/.ssh/config file
-  cat > "${TUNNEL_SSH_CONFIG_FILE}" <<EOF
-
+  cat > "${ssh_config_file}" <<EOF
 Host jump
     Hostname 127.0.0.1
     Port ${TUNNEL_LOCAL_PORT}
@@ -95,9 +95,6 @@ Host jump
     IdentitiesOnly yes
     User ${DAOS_AZ_ARM_ADMIN_USER}
     IdentityFile ${DAOS_AZ_SSH_ADMIN_KEY}
-    ControlPath ~/.ssh/ctrl/%r@%h:%p
-    ControlMaster auto
-    ControlPersist yes
 
 Host ${TUNNEL_HOSTNAME_PREFIX}*
     ProxyJump jump
@@ -109,6 +106,19 @@ Host ${TUNNEL_HOSTNAME_PREFIX}*
     IdentityFile ${DAOS_AZ_SSH_ADMIN_KEY}
 
 EOF
+}
+
+print_ssh_config() {
+  local tmp_dir
+  tmp_dir=$(mktemp -d)
+  local tmp_file
+  tmp_file="${tmp_dir}/ssh_config"
+
+  create_ssh_config "${tmp_file}"
+  cat "${tmp_file}"
+  if [[ -d "${tmp_dir}" ]]; then
+    rm -rf "${tmp_dir}"
+  fi
 }
 
 delete_ssh_config() {
@@ -146,10 +156,10 @@ create_tunnel() {
     --resource-port "22" \
     --port "${TUNNEL_LOCAL_PORT}" >/dev/null 2>&1 &
 
-  create_ssh_config
+  log.info "Creating SSH config: '${TUNNEL_SSH_CONFIG_FILE}'"
+  create_ssh_config "${TUNNEL_SSH_CONFIG_FILE}"
 
   log.info "Tunnel through '${TUNNEL_FIRST_VM_NAME}' is now running on 127.0.0.1:${TUNNEL_LOCAL_PORT}"
-  log.info "To log in run: ssh ${TUNNEL_FIRST_VM_NAME}"
 }
 
 list_tunnels() {
@@ -193,6 +203,10 @@ opts() {
       fi
       shift 2
       ;;
+    --print | -p)
+      TUNNEL_ACTION="print"
+      break
+      ;;
     --list | -l)
       TUNNEL_ACTION="list"
       shift
@@ -233,6 +247,9 @@ main() {
   vmss_check
   configure_ssh
   case "${TUNNEL_ACTION}" in
+    print)
+      print_ssh_config
+      ;;
     list)
       list_tunnels
       ;;
